ca9c77bae05f874b5ee1a8bf3b1dffd6447dd6a3
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-09-07 23:51:35 -0400

    Updating a query that is more dynamic and works.

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ rdfStore.create((err, store) => {
 
     const rdf = fs.readFileSync(__dirname + '/rdf-store/covid-19.ttl').toString();
     store.load('text/turtle', rdf, (s, d) => {
-        store.execute("SELECT * WHERE { ?s ?p ?o } LIMIT 3", (success, results) => {
-            console.log('THe results are:', results);
+        const query = `PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                       PREFIX dbo: <http://www.w3.org/1999/02/22-rdf-syntax-ns/>
+                       SELECT ?type WHERE {
+                         ?covid foaf:name "Covid-19"@en .
+                         ?covid dbo:type ?type .
+                       }
+        `
+        store.execute(query, (success, results) => {
+            console.log('The results are:', results);
         });
     });
 });
