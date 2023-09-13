bdd3f3bcb8430d2c09221782b3668ab7fdfda4ed
Simeon Ikudabo <simeonikudabo@Simeons-MacBook-Air.local>
2023-09-13 17:10:08 -0400

    adding asymptomatic case data.

--- api/helpers/handleQuery.js
+++ api/helpers/handleQuery.js
@@ -377,6 +377,33 @@ const handleQuery = async (questionType, res) => {
         return;
     }
 
+    else if (questionType === 'asymptomatic illness') {
+        rdfStore.create((err, store) => {
+            if (err) {
+                console.log('Error:', err.message);
+                res.status(200).json({ answer: 'There was an error retreiving that answer. Please try again', isSuccess: false });
+            }
+        
+            const rdf = fs.readFileSync(__dirname + '/covid-19.ttl').toString();
+            store.load('text/turtle', rdf, (s, d) => {
+                const query = `PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+                               PREFIX dbd: <https://www.doctro.com/ontology/>
+                               PREFIX dbo: <http://www.w3.org/1999/02/22-rdf-syntax-ns/>
+                               PREFIX dbr: <http://purl.org/dc/elements/1.1/>
+                               SELECT ?asymptomaticPercentage WHERE {
+                                 ?covid foaf:name "Covid-19"@en .
+                                 ?covid dbo:asymptomaticPercentage ?asymptomaticPercentage .
+                               }
+                `;
+                store.execute(query, (success, results) => {
+                    const answer = `Close to ${results[0].asymptomaticPercentage.value} or 1 out of 3 people are asymptomatic after being infected with Covid-19.`;
+                    res.status(200).json({ answer, isSuccess: true });
+                });
+            });
+        });
+        return;
+    }
+
     else {
         res.status(200).json({ answer: 'I could not find an answer to that question', isSuccess: true });
     }
